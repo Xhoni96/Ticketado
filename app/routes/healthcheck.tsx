ba75f751af7836523a55496d0c35e07f26a187b0
xhoni96 <xhonikomini96@gmail.com>
2023-01-25 13:53:23 +0100

    healthcheck changes. db setup on deploy script

--- app/routes/healthcheck.tsx
+++ app/routes/healthcheck.tsx
@@ -3,23 +3,23 @@ import type { LoaderArgs } from "@remix-run/node";
 
 // import { prisma } from "~/db.server";
 
-// export async function loader({ request }: LoaderArgs) {
-//   const host =
-//     request.headers.get("X-Forwarded-Host") ?? request.headers.get("host");
+export async function loader({ request }: LoaderArgs) {
+  //   const host =
+  //     request.headers.get("X-Forwarded-Host") ?? request.headers.get("host");
 
-//   try {
-//     const url = new URL("/", `http://${host}`);
-//     // if we can connect to the database and make a simple query
-//     // and make a HEAD request to ourselves, then we're good.
-//     await Promise.all([
-//       prisma.user.count(),
-//       fetch(url.toString(), { method: "HEAD" }).then((r) => {
-//         if (!r.ok) return Promise.reject(r);
-//       }),
-//     ]);
-//     return new Response("OK");
-//   } catch (error: unknown) {
-//     console.log("healthcheck ❌", { error });
-//     return new Response("ERROR", { status: 500 });
-//   }
-// }
+  try {
+    //   const url = new URL("/", `http://${host}:10704/server/status/alive`);
+    const url = "http://localhost:10704/server/status/alive";
+    //   if we can connect to the database and make a simple query
+    //   and make a HEAD request to ourselves, then we're good.
+    //   await Promise.all([
+
+    const res = await fetch(url);
+
+    if (res.status === 200) throw "instance is not alive";
+
+    return new Response("OK");
+  } catch (error: unknown) {
+    return new Response("ERROR", { status: 500 });
+  }
+}
