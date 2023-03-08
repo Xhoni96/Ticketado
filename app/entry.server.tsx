1260af63f24cd84cf56ac01a7dd574165c6e1757
xhoni96 <xhonikomini96@gmail.com>
2023-03-08 22:54:13 +0100

    changes for verceel

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,48 +1,59 @@
-import { PassThrough } from "stream";
+// import { PassThrough } from "stream";
 import type { EntryContext } from "@remix-run/node";
-import { Response } from "@remix-run/node";
+// import { Response } from "@remix-run/node";
 import { RemixServer } from "@remix-run/react";
-import isbot from "isbot";
-import { renderToPipeableStream } from "react-dom/server";
+// import isbot from "isbot";
+// import { renderToPipeableStream } from "react-dom/server";
+import handleRequest from "@vercel/remix-entry-server";
 
-const ABORT_DELAY = 5000;
+// const ABORT_DELAY = 5000;
 
-export default function handleRequest(
+// export default function handleRequest(
+//   request: Request,
+//   responseStatusCode: number,
+//   responseHeaders: Headers,
+//   remixContext: EntryContext
+// ) {
+//   const callbackName = isbot(request.headers.get("user-agent")) ? "onAllReady" : "onShellReady";
+
+//   return new Promise((resolve, reject) => {
+//     let didError = false;
+
+//     const { pipe, abort } = renderToPipeableStream(<RemixServer context={remixContext} url={request.url} />, {
+//       [callbackName]: () => {
+//         const body = new PassThrough();
+
+//         responseHeaders.set("Content-Type", "text/html");
+
+//         resolve(
+//           new Response(body, {
+//             headers: responseHeaders,
+//             status: didError ? 500 : responseStatusCode,
+//           })
+//         );
+
+//         pipe(body);
+//       },
+//       onShellError: (err: unknown) => {
+//         reject(err);
+//       },
+//       onError: (error: unknown) => {
+//         didError = true;
+
+//         console.error(error);
+//       },
+//     });
+
+//     setTimeout(abort, ABORT_DELAY);
+//   });
+// }
+
+export default function (
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
   remixContext: EntryContext
 ) {
-  const callbackName = isbot(request.headers.get("user-agent")) ? "onAllReady" : "onShellReady";
-
-  return new Promise((resolve, reject) => {
-    let didError = false;
-
-    const { pipe, abort } = renderToPipeableStream(<RemixServer context={remixContext} url={request.url} />, {
-      [callbackName]: () => {
-        const body = new PassThrough();
-
-        responseHeaders.set("Content-Type", "text/html");
-
-        resolve(
-          new Response(body, {
-            headers: responseHeaders,
-            status: didError ? 500 : responseStatusCode,
-          })
-        );
-
-        pipe(body);
-      },
-      onShellError: (err: unknown) => {
-        reject(err);
-      },
-      onError: (error: unknown) => {
-        didError = true;
-
-        console.error(error);
-      },
-    });
-
-    setTimeout(abort, ABORT_DELAY);
-  });
+  const remixServer = <RemixServer context={remixContext} url={request.url} />;
+  return handleRequest(request, responseStatusCode, responseHeaders, remixServer);
 }
