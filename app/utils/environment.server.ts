ad1e9d41f287a0c1f29b6af526f27a0d1b56328d
xhoni96 <xhonikomini96@gmail.com>
2023-08-12 23:04:09 +0200

    env example updated

--- app/utils/environment.server.ts
+++ app/utils/environment.server.ts
@@ -3,8 +3,6 @@ import { z } from "zod";
 const environmentSchema = z.object({
   NODE_ENV: z.enum(["development", "production"]).default("development"),
   SESSION_SECRET: z.string().min(1, { message: "SESSION_SECRET must be set" }),
-  VMT_TOKEN: z.string().min(1),
-  API_URL: z.string().min(1),
 });
 
 const environment = environmentSchema.parse(process.env);
